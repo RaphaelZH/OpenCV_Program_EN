4e220e723f61d5cb39729661f22cc55dfb39426e
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2023-07-30 23:11:38 +0200

    Update file_converter.py

--- file_converter.py
+++ file_converter.py
@@ -6,6 +6,15 @@ import pandas as pd
 from pathlib import Path
 import pytz
 
+def notebook_selector(path_object):
+    return [
+        file.name
+        for file in path_object.iterdir()
+        if (
+            file.name.split(".")[-1] == "ipynb"
+            and file.name.find("(Compressed)") == -1
+        )
+    ]
 
 def dataframe_creation():
     global courses_list
@@ -16,20 +25,17 @@ def dataframe_creation():
     st_mtime_list = []
     for course in courses_list:
         path_object = Path(course.join(dir_notebook))
-        for file in path_object.iterdir():
-            if (
-                file.name.split(".")[-1] == "ipynb"
-                and file.name.find("(Compressed)") == -1
-            ):
-                file_path_list.append(course.join(dir_notebook))
-                file_name_list.append(file.name)
-                file_object = Path(course.join(dir_notebook) + file.name)
-                st_size_list.append(file_object.stat().st_size)
-                st_mtime_list.append(
-                    datetime.fromtimestamp(
-                        file_object.stat().st_mtime, tz=pytz.timezone("cet")
-                    )
+        file_list = notebook_selector(path_object)
+        for file in file_list:
+            file_path_list.append(course.join(dir_notebook))
+            file_name_list.append(file.name)
+            file_object = Path(course.join(dir_notebook) + file.name)
+            st_size_list.append(file_object.stat().st_size)
+            st_mtime_list.append(
+                datetime.fromtimestamp(
+                    file_object.stat().st_mtime, tz=pytz.timezone("cet")
                 )
+            )
     return pd.DataFrame(
         list(zip(file_path_list, file_name_list, st_size_list, st_mtime_list)),
         columns=["File path", "File name", "File size", "Modification date"],
@@ -39,13 +45,23 @@ def dataframe_creation():
 def file_checker(func):
     @wraps(func)
     def wrapper():
-        global csv_file
+        global csv_object
         output_filename_dict = {}
-        if csv_file.is_file():
-            df = pd.read_csv(csv_file)
+        if csv_object.is_file():
+            df = pd.read_csv(csv_object)
+            #if 
+            #folder counterobject
             for index, row in df.iterrows():
                 input_filename = row["File path"] + row["File name"]
+                folder_object = Path(row["File path"])
+                folder_counter = len([file for file in notebook_selector(folder_object)])
+                row_counter = len(df.loc[df["File path"] == row["File path"]])
+                if folder_counter != row_counter:
+                    print(folder_counter)
+                
+                
                 file_object = Path(input_filename)
+                
                 if (
                     datetime.fromtimestamp(
                         file_object.stat().st_mtime, tz=pytz.timezone("cet")
@@ -70,7 +86,7 @@ def file_checker(func):
 def compression_record(func):
     @wraps(func)
     def wrapper():
-        global csv_file
+        global csv_object
         df, output_filename_dict = func()
         if output_filename_dict != {}:
             for (index, _), (key, value) in zip(
@@ -80,7 +96,7 @@ def compression_record(func):
                     df.loc[index, "Compressed file"] = value.split("/Notebooks/")[-1]
                     file_object = Path(value)
                     df.loc[index, "Compressed size"] = file_object.stat().st_size
-        df.to_csv(csv_file, index=False)
+        df.to_csv(csv_object, index=False)
 
     return wrapper
 
@@ -101,6 +117,6 @@ courses_list = [
     "Modern Computer Vision PyTorch, TensorFlow 2 Keras & OpenCV 4",
 ]
 
-csv_file = Path("modification_record.csv")
+csv_object = Path("modification_record.csv")
 
 file_generator()
