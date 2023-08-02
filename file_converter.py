1ec2c62d01a3a1cd06ca7e580465a7a02e343e94
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2023-08-02 19:48:51 +0200

    update

--- file_converter.py
+++ file_converter.py
@@ -18,8 +18,8 @@ def notebook_selector(path_object):
 
 
 def info_collector(course, file, info_dict):
-    dir_notebook = ["./", "/Notebooks/"]
-    info_dict["File path"].append(course.join(dir_notebook))
+    global dir_notebook
+    info_dict["File path"].append(course)
     info_dict["File name"].append(file)
     file_object = Path(course.join(dir_notebook) + file)
     info_dict["File size"].append(file_object.stat().st_size)
@@ -30,8 +30,7 @@ def info_collector(course, file, info_dict):
 
 
 def dataframe_creation():
-    global courses_list
-    dir_notebook = ["./", "/Notebooks/"]
+    global courses_list, dir_notebook
     info_dict = {
         "File path": [],
         "File name": [],
@@ -46,43 +45,55 @@ def dataframe_creation():
     return pd.DataFrame.from_dict(data=info_dict)
 
 
+def date_format(time):
+    return datetime.fromtimestamp(time, tz=pytz.timezone("cet"))
+
+
 def file_checker(func):
     @wraps(func)
     def wrapper():
-        global csv_object
+        global courses_list, csv_object, dir_notebook
         output_filename_dict = {}
         if csv_object.is_file():
             df = pd.read_csv(csv_object)
-            # if
-            # folder counterobject
-            for index, row in df.iterrows():
-                input_filename = row["File path"] + row["File name"]
-                folder_object = Path(row["File path"])
-                folder_counter = len(
-                    [file for file in notebook_selector(folder_object)]
-                )
-                row_counter = len(df.loc[df["File path"] == row["File path"]])
-                if folder_counter != row_counter:
-                    print(folder_counter)
-
-                file_object = Path(input_filename)
-
-                if (
-                    datetime.fromtimestamp(
-                        file_object.stat().st_mtime, tz=pytz.timezone("cet")
-                    )
-                    != row["Modification date"]
-                    and file_object.stat().st_size != row["File size"]
-                ):
-                    df.loc[index, "Modification date"] = datetime.fromtimestamp(
-                        file_object.stat().st_mtime, tz=pytz.timezone("cet")
-                    )
-                    output_filename_dict[index] = func(input_filename)
+            info_dict = df.to_dict("list")
+            for course in courses_list:
+                path_object = Path(course.join(dir_notebook))
+                path_counter = len(list(notebook_selector(path_object)))
+                if path_counter != len(df.loc[df["File path"] == course]):
+                    for file in notebook_selector(path_object):
+                        input_filename = Path(course.join(dir_notebook) + file)
+                        file_object = Path(input_filename)
+                        if file not in df.loc[df["File path"] == course]["File name"]:
+                            info_dict = info_collector(course, file, info_dict)
+                            #df = pd.DataFrame.from_dict(data=info_dict)
+                            
+                        print(date_format(file_object.stat().st_mtime))
+                        """
+                        elif (
+                            date_format(file_object.stat().st_mtime)
+                            != df.loc[
+                                df["File path"] == course and df["File name"] == file
+                            ]["Modification date"]
+                            and file_object.stat().st_size
+                            != df.loc[
+                                df["File path"] == course and df["File name"] == file
+                            ]["File size"]
+                        ):
+                            df.loc[
+                                df["File path"] == course and df["File name"] == file
+                            ]["Modification date"] = date_format(
+                                file_object.stat().st_mtime
+                            )
+                            df.loc[
+                                df["File path"] == course and df["File name"] == file
+                            ]["File size"] = file_object.stat().st_size
+                        """
         else:
             df = dataframe_creation()
-            for index, row in df.iterrows():
-                input_filename = row["File path"] + row["File name"]
-                output_filename_dict[index] = func(input_filename)
+        for index, row in df.iterrows():
+            input_filename = row["File path"].join(dir_notebook) + row["File name"]
+            output_filename_dict[index] = func(input_filename)
         return df, output_filename_dict
 
     return wrapper
@@ -94,9 +105,7 @@ def compression_record(func):
         global csv_object
         df, output_filename_dict = func()
         if output_filename_dict != {}:
-            for (index, _), (key, value) in zip(
-                df.iterrows(), output_filename_dict.items()
-            ):
+            for index, (key, value) in zip(df.index, output_filename_dict.items()):
                 if index == key:
                     df.loc[index, "Compressed file"] = value.split("/Notebooks/")[-1]
                     file_object = Path(value)
@@ -111,9 +120,7 @@ def compression_record(func):
 def file_generator(input_filename):
     os.system(f"jupyter nbconvert --to html '{input_filename}'")
     output_filename = " (Compressed).ipynb".join(input_filename.split(".ipynb"))
-    compress(
-        input_filename, output_filename=output_filename, img_width=800, img_format="png"
-    )
+    compress(input_filename, output_filename, img_width=800, img_format="png")
     return output_filename
 
 
@@ -123,5 +130,7 @@ courses_list = [
 ]
 
 csv_object = Path("modification_record.csv")
+
+dir_notebook = ["./", "/Notebooks/"]
 
 file_generator()
