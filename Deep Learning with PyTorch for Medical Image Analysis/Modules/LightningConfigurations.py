6d79a36258aee0560bf8f2d1ae76fcfe29442783
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2023-10-21 21:41:08 +0200

    class CustomProgressBar

--- Deep Learning with PyTorch for Medical Image Analysis/Modules/LightningConfigurations.py
+++ Deep Learning with PyTorch for Medical Image Analysis/Modules/LightningConfigurations.py
@@ -4,29 +4,39 @@ from lightning.pytorch.callbacks.progress.rich_progress import RichProgressBarTh
 from .CustomColors import *
 
 
-def custom_gradient_colors(colors, color_index):
-    color_list = gradient_dark_generator(colors[color_index], 3)
-    color_list.append(colors[color_index])
-    color_list.remove(color_list[0])
-    return color_list
+class CustomProgressBar(RichProgressBar):
+    def __init__(self):
+        super().__init__()
+        self.enable = True
 
+        def custom_gradient_colors(colors, color_index):
+            color_list = gradient_dark_generator(colors[color_index], 3)
+            color_list.append(colors[color_index])
+            color_list.remove(color_list[0])
+            return color_list
 
-def custom_progress_bar():
-    colors = calm_color_generator(2)
-    color_group_1 = custom_gradient_colors(colors, 0)
-    color_group_2 = custom_gradient_colors(colors, 1)
-    return RichProgressBar(
-        theme=RichProgressBarTheme(
-            description=color_group_1[2],
-            progress_bar=color_group_1[0],
-            progress_bar_finished=color_group_1[1],
-            progress_bar_pulse=color_group_1[1],
-            batch_progress=color_group_1[2],
-            time=color_group_2[1],
-            processing_speed=color_group_2[0],
-            metrics=color_group_2[2],
+        self.colors = calm_color_generator(2)
+        self.color_group_1 = custom_gradient_colors(self.colors, 0)
+        self.color_group_2 = custom_gradient_colors(self.colors, 1)
+
+        self.theme = RichProgressBarTheme(
+            description=self.color_group_1[2],
+            progress_bar=self.color_group_1[0],
+            progress_bar_finished=self.color_group_1[1],
+            progress_bar_pulse=self.color_group_1[1],
+            batch_progress=self.color_group_1[2],
+            time=self.color_group_2[1],
+            processing_speed=self.color_group_2[0],
+            metrics=self.color_group_2[2],
             metrics_text_delimiter="\n",
-            metrics_format="<7n",
-        ),
-        leave=False,
-    )
+            metrics_format="<.5f",
+        )
+        self.leave = (False,)
+
+    def disable(self):
+        self.enable = False
+
+    def get_metrics(self, trainer, model):
+        items = super().get_metrics(trainer, model)
+        items.pop("v_num", None)
+        return items
--- Deep Learning with PyTorch for Medical Image Analysis/Modules/__init__.py
+++ Deep Learning with PyTorch for Medical Image Analysis/Modules/__init__.py
@@ -1,3 +1,3 @@
 from .CustomColors import *
-from .LightningConfigurations import custom_progress_bar
-from .Tabulation import DataFrame_Generator, Form_Generator
+from .LightningConfigurations import *
+from .Tabulation import *
