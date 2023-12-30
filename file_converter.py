de32dcdc77c193e9557d4f4bfe24aa806572e0ab
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2023-12-30 22:58:09 +0100

    nbformat normalize

--- file_converter.py
+++ file_converter.py
@@ -135,10 +135,27 @@ def compression_record(func):
 
     return wrapper
 
+import nbformat
 
 @compression_record
 @file_checker
 def file_generator(input_filename):
+    with open(input_filename, "r") as file:
+        nb_corrupted = nbformat.reader.read(file)
+
+    nbformat.validator.validate(nb_corrupted)
+# <stdin>:1: MissingIDFieldWarning: Code cell is missing an id field, 
+# this will become a hard error in future nbformat versions. 
+# You may want to use `normalize()` on your notebooks before validations (available since nbformat 5.1.4). 
+# Previous versions of nbformat are fixing this issue transparently, and will stop doing so in the future.
+
+    nb_fixed = nbformat.validator.normalize(nb_corrupted)
+    nbformat.validator.validate(nb_fixed[1])
+# Produces no warnings or errors.
+
+    with open(input_filename, "w") as file:
+        nbformat.write(nb_fixed[1], file)
+    
     output_filename = " (Compressed).ipynb".join(input_filename.split(".ipynb"))
     compress(input_filename, output_filename, img_width=800, img_format="png")
     return output_filename
