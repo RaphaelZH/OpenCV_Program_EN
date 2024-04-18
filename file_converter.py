91b9a837284518aee0219877f850a15511c1e287
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2024-04-18 23:34:37 +0200

    updates

--- file_converter.py
+++ file_converter.py
@@ -9,19 +9,21 @@ import pytz
 
 def notebook_selector(path_object):
     return [
-        file.name
-        for file in path_object.iterdir()
+        file_object.name
+        for file_object in path_object.iterdir()
         if (
-            file.name.split(".")[-1] == "ipynb" and file.name.find("(Compressed)") == -1
+            file_object.name.split(".")[-1] == "ipynb"
+            and file_object.name.find("(Compressed)") == -1
         )
     ]
 
 
-def info_collector(course, file, info_dict):
+def info_collector(course, subpath, file_name, info_dict):
     global dir_notebook
     info_dict["File path"].append(course)
-    info_dict["File name"].append(file)
-    file_object = Path(course.join(dir_notebook) + file)
+    info_dict["File name"].append(file_name)
+    path_object = Path(course.join(dir_notebook))
+    file_object = Path(f"{subpath}/" + file_name)
     info_dict["File size"].append(file_object.stat().st_size)
     info_dict["Modification date"].append(
         datetime.fromtimestamp(file_object.stat().st_mtime, tz=pytz.timezone("cet"))
@@ -39,9 +41,11 @@ def dataframe_creation():
     }
     for course in courses_list:
         path_object = Path(course.join(dir_notebook))
-        file_list = notebook_selector(path_object)
-        for file in file_list:
-            info_dict = info_collector(course, file, info_dict)
+        for subpath_object in path_object.iterdir():
+            subpath = subpath_object.expanduser()
+            file_list = notebook_selector(subpath_object)
+            for file_name in file_list:
+                info_dict = info_collector(course, subpath, file_name, info_dict)
     return pd.DataFrame.from_dict(data=info_dict)
 
 
@@ -49,16 +53,18 @@ def date_format(time):
     return datetime.fromtimestamp(time, tz=pytz.timezone("cet"))
 
 
-def alteration_monitor(file_object, cell_time, cell_size):
+def alteration_monitor(path_object, cell_time, cell_size):
     global alteration
-    if file_object.stat().st_size != cell_size.item():
-        cell_time = date_format(file_object.stat().st_mtime)
-        cell_size = file_object.stat().st_size
-        alteration = True
-        return cell_time, cell_size
-    else:
-        alteration = False
-        return cell_time.values[0], cell_size.values[0]
+    for file_object in path_object.iterdir():
+        print(file_object)
+        if file_object.stat().st_size != cell_size.item():
+            cell_time = date_format(file_object.stat().st_mtime)
+            cell_size = file_object.stat().st_size
+            alteration = True
+            return cell_time, cell_size
+        else:
+            alteration = False
+            return cell_time.values[0], cell_size.values[0]
 
 
 def notebook_normalize(input_filename):
@@ -81,47 +87,61 @@ def file_checker(func):
             info_dict = df.to_dict("list")
             for course in courses_list:
                 path_object = Path(course.join(dir_notebook))
-                for file in notebook_selector(path_object):
-                    input_filename = course.join(dir_notebook) + file
-                    file_object = Path(input_filename)
-                    if (
-                        file
-                        not in df.loc[df["File path"] == course]["File name"].to_list()
-                    ):
-                        info_dict = info_collector(course, file, info_dict)
-                        info_dict["Compressed file"].append("")
-                        info_dict["Compressed size"].append("")
-                        df = pd.DataFrame.from_dict(data=info_dict)
-                        index = df.shape[0] - 1
-                        output_filename_dict[index] = func(input_filename)
-                    else:
-                        df.loc[
-                            (df["File path"] == course) & (df["File name"] == file),
-                            ["Modification date", "File size"],
-                        ] = alteration_monitor(
-                            file_object,
+                for subpath_object in path_object.iterdir():
+                    subpath = subpath_object.expanduser()
+                    file_list = notebook_selector(subpath_object)
+                    for file_name in file_list:
+                        if (
+                            file_name
+                            not in df.loc[df["File path"] == course][
+                                "File name"
+                            ].to_list()
+                        ):
+                            info_dict = info_collector(
+                                course, subpath, file_name, info_dict
+                            )
+                            info_dict["Compressed file"].append("")
+                            info_dict["Compressed size"].append("")
+                            df = pd.DataFrame.from_dict(data=info_dict)
+                            index = df.shape[0] - 1
+                            output_filename_dict[index] = func(
+                                subpath + "/" + file_name
+                            )
+                        else:
                             df.loc[
-                                (df["File path"] == course) & (df["File name"] == file),
-                                "Modification date",
-                            ],
-                            df.loc[
-                                (df["File path"] == course) & (df["File name"] == file),
-                                "File size",
-                            ],
-                        )
-                        if alteration:
-                            for index, row in df.iterrows():
-                                if (
-                                    input_filename
-                                    == row["File path"].join(dir_notebook)
-                                    + row["File name"]
-                                ):
-                                    output_filename_dict[index] = func(input_filename)
+                                (df["File path"] == course)
+                                & (df["File name"] == file_name),
+                                ["Modification date", "File size"],
+                            ] = alteration_monitor(
+                                subpath_object,
+                                df.loc[
+                                    (df["File path"] == course)
+                                    & (df["File name"] == file_name),
+                                    "Modification date",
+                                ],
+                                df.loc[
+                                    (df["File path"] == course)
+                                    & (df["File name"] == file_name),
+                                    "File size",
+                                ],
+                            )
+                            if alteration:
+                                for index, row in df.iterrows():
+                                    if (
+                                        subpath
+                                        == row["File path"].join(dir_notebook)
+                                        + row["File name"].split(".")[0]
+                                    ):
+                                        output_filename_dict[index] = func(
+                                            subpath + "/" + file_name
+                                        )
         else:
             df = dataframe_creation()
             for index, row in df.iterrows():
-                input_filename = row["File path"].join(dir_notebook) + row["File name"]
-                output_filename_dict[index] = func(input_filename)
+                subpath = (
+                    row["File path"].join(dir_notebook) + row["File name"].split(".")[0]
+                )
+                output_filename_dict[index] = func(subpath + "/" + row["File name"])
         return df, output_filename_dict
 
     return wrapper
@@ -161,7 +181,7 @@ alteration = False
 courses_list = [
     "Deep Learning with PyTorch for Medical Image Analysis",
     "Modern Computer Vision PyTorch, TensorFlow 2 Keras & OpenCV 4",
-    ""
+    "PyTorch for Deep Learning and Computer Vision",
 ]
 
 csv_object = Path("modification_record.csv")
